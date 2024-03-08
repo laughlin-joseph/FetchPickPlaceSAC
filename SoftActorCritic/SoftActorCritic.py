490dbaa94bebab254db715bfdd73bfe00b593bd5
Joe Laughlin <laughlin.joseph@gmail.com>
2024-03-07 20:58:02 -0600

    Final control code update for the day. I'm hungry.

--- SoftActorCritic/SoftActorCritic.py
+++ SoftActorCritic/SoftActorCritic.py
@@ -1,97 +1,83 @@
 import gymnasium as gym
-import math
-import numpy as np
 from RLLib.Agents.SAC.Agent import SACAgent
 from RLLib.Agents.PPO.Agent import PPOAgent
 
-Agent=None
-env_str='CartPole-v1'
-hidden=[512,512]
-epoch_count=200
-epoch_steps=12500
-ep_len=50
-w_HER=False
-HER_obs_proc=lambda obs: None
-HER_rew_function=lambda exp :0
-w_PER=True
-start_steps=12500
-train_agent=True
-test_agent=True
-log_agent=True
-test_every=10
+train_agent = True
 
-#env_str='HandManipulateBlock-v1'
-#hidden=[512,512,512]
-#epoch_count=200
-#epoch_steps=5000
-#ep_len=50
-#w_HER=True
-#HER_obs_proc=lambda obs: None
-#HER_rew_function=lambda exp :0
-#w_PER=False
-#start_steps=25000
-#train_agent=True
-#test_agent=True
-#log_agent=True
-#test_every=10
+ppo_env_str='CartPole-v1'
+ppo_hidden=[512,512]
+ppo_epoch_count=200
+ppo_epoch_steps=12500
+ppo_ep_len=250
+ppo_w_HER=False
+ppo_HER_obs_proc=lambda obs: None
+ppo_HER_rew_function=lambda exp :0
+ppo_w_PER=True
+ppo_start_steps=12500
+ppo_test_agent=True
+ppo_log_agent=True
+ppo_test_every=10
 
-#env_str='FetchPickAndPlace-v2'
-#hidden=[512,512]
-#epoch_count=200
-#epoch_steps=5000
-#ep_len=50
-#w_HER=True
-#HER_obs_proc=lambda obs: None
-#HER_rew_function=lambda exp :0
-#w_PER=False
-#start_steps=25000
-#train_agent=True
-#test_agent=True
-#log_agent=True
-#test_every=10
+sac_env_str='FetchPickAndPlace-v2'
+sac_hidden=[512,512]
+sac_epoch_count=200
+sac_epoch_steps=5000
+sac_ep_len=50
+sac_w_HER=True
+sac_HER_obs_proc=lambda obs: None
+sac_HER_rew_function=lambda exp :0
+sac_w_PER=False
+sac_start_steps=25000
+sac_test_agent=True
+sac_log_agent=True
+sac_test_every=10
 
-#Adroit Hand Params
-#env_str='AdroitHandDoorSparse-v1'
-#hidden=[256,512,512,256]
-#epoch_count=200
-#epoch_steps=20000
-#ep_len=200
-#w_HER=True
-#HER_obs_proc=lambda obs : (np.array([(math.pi/2)]),np.array([obs[28]]))
-#HER_rew_function=lambda exp : 5 qq
-#w_PER=True
-#start_steps=100000
-#train_agent=True
-#test_agent=True
-#log_agent=True
-#test_every=10
+#sac_env_str='HandManipulateBlockRotateXYZ-v1'
+#sac_hidden=[512,512,512]
+#sac_epoch_count=200
+#sac_epoch_steps=5000
+#sac_ep_len=50
+#sac_w_HER=True
+#sac_HER_obs_proc=lambda obs: None
+#sac_HER_rew_function=lambda exp :0
+#sac_w_PER=False
+#sac_start_steps=25000
+#sac_test_agent=True
+#sac_log_agent=True
+#sac_test_every=10
 
-env=gym.make(env_str, max_episode_steps=ep_len)
+ppo_env=gym.make(ppo_env_str, max_episode_steps=ppo_ep_len)
+sac_env=gym.make(sac_env_str, max_episode_steps=sac_ep_len)
 
 if train_agent:
-    #Agent=SACAgent(env, 
-    #                 hidden_sizes=hidden,
-    #                 epochs=epoch_count,
-    #                 steps_per_epoch=epoch_steps,
-    #                 max_ep_len=ep_len,
-    #                 use_HER=w_HER,
-    #                 use_PER=w_PER,
-    #                 HER_obs_pr=HER_obs_proc,
-    #                 HER_rew_func=HER_rew_function,
-    #                 start_exploration_steps=start_steps,
-    #                 run_tests_and_record=test_agent, 
-    #                 enable_logging=log_agent,
-    #                 test_every_epochs=test_every)
+    PPO_Agent=PPOAgent(
+                    ppo_env, 
+                    hidden_sizes=ppo_hidden,
+                    epochs=ppo_epoch_count,
+                    steps_per_epoch=ppo_epoch_steps,
+                    max_ep_len=ppo_ep_len,
+                    run_tests_and_record=ppo_test_agent, 
+                    enable_logging=ppo_log_agent,
+                    test_every_epochs=ppo_test_every
+                    )
     
-    Agent=PPOAgent(env, 
-                     hidden_sizes=hidden,
-                     epochs=epoch_count,
-                     steps_per_epoch=epoch_steps,
-                     max_ep_len=ep_len,
-                     run_tests_and_record=test_agent, 
-                     enable_logging=log_agent,
-                     test_every_epochs=test_every)
+    SAC_Agent=SACAgent(
+                    sac_env, 
+                    hidden_sizes=sac_hidden,
+                    epochs=sac_epoch_count,
+                    steps_per_epoch=sac_epoch_steps,
+                    max_ep_len=sac_ep_len,
+                    use_HER=sac_w_HER,
+                    use_PER=sac_w_PER,
+                    HER_obs_pr=sac_HER_obs_proc,
+                    HER_rew_func=sac_HER_rew_function,
+                    start_exploration_steps=sac_start_steps,
+                    run_tests_and_record=sac_test_agent, 
+                    enable_logging=sac_log_agent,
+                    test_every_epochs=sac_test_every)
     
-    Agent.train()
+    PPO_Agent.train()
+    #SAC_Agent.train()
 
-env.close()
+ppo_env.close()
+#sac_env.close()
