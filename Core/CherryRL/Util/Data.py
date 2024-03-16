e2acec06123acec313ab44c07402632ecb72b6aa
Joe Laughlin <laughlin.joseph@gmail.com>
2024-03-15 20:32:33 -0500

    Small update.

--- Core/CherryRL/Util/Data.py
+++ Core/CherryRL/Util/Data.py
@@ -82,11 +82,6 @@ class GoalUpdateStrategy(Enum):
     EPISODE = 3
 
 class PPOBuffer:
-    """
-    A buffer for storing trajectories experienced by a PPO agent interacting
-    with the environment, and using Generalized Advantage Estimation (GAE-Lambda)
-    for calculating the advantages of state-action pairs.
-    """
 
     def __init__(self, obs_dim, act_dim, size, device, gamma=0.99, lam=0.95):
         self.obs_buf = np.zeros(funcs.combined_shape(size, obs_dim), dtype=np.float32)
@@ -100,9 +95,6 @@ class PPOBuffer:
         self.ptr, self.path_start_idx, self.max_size, self.device = 0, 0, size, device
 
     def store(self, obs, act, rew, val, logp):
-        """
-        Append one timestep of agent-environment interaction to the buffer.
-        """
         assert self.ptr < self.max_size     # buffer has to have room so you can store
         self.obs_buf[self.ptr] = obs
         self.act_buf[self.ptr] = act
@@ -112,43 +104,23 @@ class PPOBuffer:
         self.ptr += 1
 
     def finish_path(self, last_val=0):
-        """
-        Call this at the end of a trajectory, or when one gets cut off
-        by an epoch ending. This looks back in the buffer to where the
-        trajectory started, and uses rewards and value estimates from
-        the whole trajectory to compute advantage estimates with GAE-Lambda,
-        as well as compute the rewards-to-go for each state, to use as
-        the targets for the value function.
-
-        The "last_val" argument should be 0 if the trajectory ended
-        because the agent reached a terminal state (died), and otherwise
-        should be V(s_T), the value function estimated for the last state.
-        This allows us to bootstrap the reward-to-go calculation to account
-        for timesteps beyond the arbitrary episode horizon (or epoch cutoff).
-        """
-
         path_slice = slice(self.path_start_idx, self.ptr)
         rews = np.append(self.rew_buf[path_slice], last_val)
         vals = np.append(self.val_buf[path_slice], last_val)
         
-        # the next two lines implement GAE-Lambda advantage calculation
+        #GAE-Lambda advantage calculation
         deltas = rews[:-1] + self.gamma * vals[1:] - vals[:-1]
         self.adv_buf[path_slice] = funcs.discount_cumsum(deltas, self.gamma * self.lam)
         
-        # the next line computes rewards-to-go, to be targets for the value function
+        #Computes rewards-to-go targets for the value function.
         self.ret_buf[path_slice] = funcs.discount_cumsum(rews, self.gamma)[:-1]
         
         self.path_start_idx = self.ptr
 
     def get(self):
-        """
-        Call this at the end of an epoch to get all of the data from
-        the buffer, with advantages appropriately normalized (shifted to have
-        mean zero and std one). Also, resets some pointers in the buffer.
-        """
-        assert self.ptr == self.max_size    # buffer has to be full before you can get
+        assert self.ptr == self.max_size
         self.ptr, self.path_start_idx = 0, 0
-        # the next two lines implement the advantage normalization trickS
+        #Use the advantage normalization tricks
         adv_mean  = np.mean(self.adv_buf)
         adv_std = np.std(self.adv_buf)
         self.adv_buf = (self.adv_buf - adv_mean) / adv_std
